3b8d19c04895c7628b6b9a7a0e31258ced7105f1
burnedchris <christopher@everfund.com>
2025-01-22 03:55:00 +0000

    fix: :bug: fixed path import being broken from moving to src directory

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -1,4 +1,4 @@
-import { docs, meta } from "@/.source";
+import { docs, meta } from "../../.source";
 import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
 import { icons } from "lucide-react";
